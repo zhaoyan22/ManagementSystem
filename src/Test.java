e79ffb2571e56102aadce583546549c91826c0c7
zhaoyan <642865328@qq.com>
2017-05-09 19:38:57 +0800

    采用id的关联方法，更改了文件的存储路径

--- src/Test.java
+++ src/Test.java
@@ -11,6 +11,7 @@ public class Test extends JFrame implements ActionListener {
 	private JTextField txtInput;
 	private JTextField txtRequest;
 	private JTextField txtAddLabel;
+	private JTextField txtDelete;
 	private JButton btnConfirm;
 	private JButton btnRequest;
 	private JButton btnAddLabel;
@@ -19,11 +20,7 @@ public class Test extends JFrame implements ActionListener {
 	private JTextArea txtRequestContent;
 	ArrayList<String> fileOfLabel = new ArrayList<String>();
 	ArrayList<String> checkLabel = new ArrayList<String>();
-	private JTextField txtDelete;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -43,6 +40,7 @@ public class Test extends JFrame implements ActionListener {
 	 * @throws Exception
 	 */
 	public Test() throws Exception {
+		judgeFile();
 		setTitle("\u77E5\u8BC6\u5E93\u7BA1\u7406\u8F6F\u4EF6");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 613, 497);
@@ -97,18 +95,18 @@ public class Test extends JFrame implements ActionListener {
 		btnAddLabel.addActionListener(this);
 		btnAddLabel.setBounds(189, 87, 93, 33);
 		getContentPane().add(btnAddLabel);
-		txtRecentOfContent.setText(recentOfContent("D:\\1\\content.txt"));
-		
+		txtRecentOfContent.setText(recentOfContent("src/content.txt"));
+
 		txtDelete = new JTextField();
 		txtDelete.setBounds(309, 410, 198, 27);
 		getContentPane().add(txtDelete);
 		txtDelete.setColumns(10);
-		
+
 		btnDelete = new JButton("\u5220\u9664");
 		btnDelete.addActionListener(this);
 		btnDelete.setBounds(507, 410, 71, 27);
 		getContentPane().add(btnDelete);
-		
+
 		checkBoxLayout();
 	}
 
@@ -136,14 +134,16 @@ public class Test extends JFrame implements ActionListener {
 						txtInput.grabFocus();
 						fileOfLabel.clear();
 					} else {
+						String id = creatId("src/content.txt");
 						try {
-							write(txtInput.getText(), "D:\\1\\content.txt");
+							write(id + "/" + txtInput.getText(),
+									"src/content.txt");
 						} catch (Exception e1) {
 							e1.printStackTrace();
 						}
 						try {
 							txtRecentOfContent
-									.setText(recentOfContent("D:\\1\\content.txt"));
+									.setText(recentOfContent("src/content.txt"));
 						} catch (Exception e2) {
 							e2.printStackTrace();
 						}
@@ -160,7 +160,7 @@ public class Test extends JFrame implements ActionListener {
 						}
 
 						try {
-							writeRelationship(contentLabelList(fileOfLabel));
+							writeRelationship(id, contentLabelList(fileOfLabel));
 						} catch (IOException e1) {
 							e1.printStackTrace();
 						} catch (Exception e1) {
@@ -188,19 +188,19 @@ public class Test extends JFrame implements ActionListener {
 					}
 				}
 				try {
-					request(contentLabelList(checkLabel));
+					request(checkLabelId(checkLabel));
 				} catch (Exception e1) {
 					e1.printStackTrace();
 				}
 			}
 
 		}
-		if(e.getSource()==btnDelete){
-			if(txtDelete.getText().equals("")){
+		if (e.getSource() == btnDelete) {
+			if (txtDelete.getText().equals("")) {
 				JOptionPane.showMessageDialog(null, "请输入要删除内容~", "错误",
 						JOptionPane.ERROR_MESSAGE);
-				txtDelete.grabFocus();}
-			else{
+				txtDelete.grabFocus();
+			} else {
 				try {
 					delete();
 				} catch (Exception e1) {
@@ -208,17 +208,21 @@ public class Test extends JFrame implements ActionListener {
 				}
 				try {
 					txtRecentOfContent
-					.setText(recentOfContent("D:\\1\\content.txt"));
+							.setText(recentOfContent("src/content.txt"));
 				} catch (Exception e1) {
 					e1.printStackTrace();
 				}
 			}
 		}
 	}
+
 	/**
 	 * 该方法为文件内写入需要写的内容
-	 * @param content [写入文件的内容]
-	 * @param fileName [文件的名字]
+	 * 
+	 * @param content
+	 *            [写入文件的内容]
+	 * @param fileName
+	 *            [文件的名字]
 	 * @throws Exception
 	 */
 	public void write(String content, String fileName) throws Exception {
@@ -228,10 +232,14 @@ public class Test extends JFrame implements ActionListener {
 		writeContent.newLine();
 		writeContent.close();
 	}
+
 	/**
 	 * 该方法读出文件内确定的某行内容
-	 * @param lineNumber [需要读的某行的行数]
-	 * @param fileName [文件的名字]
+	 * 
+	 * @param lineNumber
+	 *            [需要读的某行的行数]
+	 * @param fileName
+	 *            [文件的名字]
 	 * @return [方法返回读出的内容]
 	 * @throws Exception
 	 */
@@ -249,9 +257,12 @@ public class Test extends JFrame implements ActionListener {
 		read.close();
 		return s;
 	}
+
 	/**
 	 * 该方法计算文件的行数
-	 * @param fileName [需要读取的文件的名字]
+	 * 
+	 * @param fileName
+	 *            [需要读取的文件的名字]
 	 * @return [该方法返回文件的行数]
 	 * @throws Exception
 	 */
@@ -266,37 +277,49 @@ public class Test extends JFrame implements ActionListener {
 		read.close();
 		return line;
 	}
+
 	/**
 	 * 该方法读取存内容的所有内容
-	 * @param fileName [文件的名字]
+	 * 
+	 * @param fileName
+	 *            [文件的名字]
 	 * @return [返回内容文件的所有内容]
 	 * @throws Exception
 	 */
 	public String recentOfContent(String fileName) throws Exception {
 		String recentContent = "";
+		String content;
 		for (int i = numberOfLine(fileName); i > 0; i--) {
-			recentContent += readLine(i, fileName) + "\n";
+			content = readLine(i, fileName);
+			String[] contentArray = content.split("/");
+			recentContent += contentArray[1] + "\n";
 		}
 		return recentContent;
 	}
+
 	/**
 	 * 该方法将输入的标签写入的标签文件
-	 * @param fileOfLabel [输入的标签数组]
+	 * 
+	 * @param fileOfLabel
+	 *            [输入的标签数组]
 	 * @throws Exception
 	 */
 	public void insertLabel(ArrayList<String> fileOfLabel) throws Exception {
 		boolean flag = false;
+		String labelOfContent;
 		int i = 0;
-		int num = numberOfLine("D:\\1\\label.txt");
+		int num = numberOfLine("src/label.txt");
 		if (num == 0) {
 			for (i = 0; i < fileOfLabel.size(); i++) {
-				write(fileOfLabel.get(i), "D:\\1\\label.txt");
+				write(creatId("src/label.txt") + "/" + fileOfLabel.get(i),
+						"src/label.txt");
 			}
 		} else {
 			for (i = 0; i < fileOfLabel.size(); i++) {
 				for (int j = 1; j <= num; j++) {
-					flag = fileOfLabel.get(i).equals(
-							readLine(j, "D:\\1\\label.txt"));
+					labelOfContent = readLine(j, "src/label.txt");
+					String[] labelContent = labelOfContent.split("/");
+					flag = fileOfLabel.get(i).equals(labelContent[1]);
 					if (flag) {
 						break;
 					}
@@ -304,20 +327,26 @@ public class Test extends JFrame implements ActionListener {
 				if (flag) {
 					continue;
 				} else {
-					write(fileOfLabel.get(i), "D:\\1\\label.txt");
+					write(creatId("src/label.txt") + "/" + fileOfLabel.get(i),
+							"src/label.txt");
 				}
 			}
 		}
 	}
+
 	/**
 	 * 该方法给checkbox布局，有多少个标签对应多少个checkbox
+	 * 
 	 * @throws Exception
 	 */
 	public void checkBoxLayout() throws Exception {
 		Map<String, JCheckBox> radioMap = new HashMap<String, JCheckBox>();
-		for (int i = 0; i < numberOfLine("D:\\1\\label.txt"); i++) {
+		String labelContent;
+		for (int i = 0; i < numberOfLine("src/label.txt"); i++) {
 			radioMap.put("a" + i, new JCheckBox());
-			radioMap.get("a" + i).setText(readLine(i + 1, "D:\\1\\label.txt"));
+			labelContent = readLine(i + 1, "src/label.txt");
+			String[] labelContentArray = labelContent.split("/");
+			radioMap.get("a" + i).setText(labelContentArray[1]);
 			if (i % 3 == 0) {
 				radioMap.get("a" + i).setBounds(309, 91 + (i / 3) * 30, 76, 25);
 			} else if (i % 3 == 1) {
@@ -330,26 +359,12 @@ public class Test extends JFrame implements ActionListener {
 			getContentPane().add(radioMap.get("a" + i));
 		}
 	}
-	/**
-	 * 该方法得到一个标签在标签文件的序列号
-	 * @param label [需要查的标签]
-	 * @return [返回需要查的标签的序列号]
-	 * @throws Exception
-	 */
-	public int getLabelIndex(String label) throws Exception {
-		int Index = 0;
-		int num = numberOfLine("D:\\1\\label.txt");
-		for (int i = 1; i <= num; i++) {
-			if (label.equals(readLine(i, "D:\\1\\label.txt"))) {
-				Index = i;
-				break;
-			}
-		}
-		return Index;
-	}
+
 	/**
 	 * 该方法将输入的标签存到数组内将重复的标签去掉
-	 * @param fileOfLabel [输入的标签数组]
+	 * 
+	 * @param fileOfLabel
+	 *            [输入的标签数组]
 	 * @return [返回没有重复标签的数组]
 	 * @throws Exception
 	 */
@@ -357,20 +372,49 @@ public class Test extends JFrame implements ActionListener {
 			throws Exception {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		for (int i = 0; i < fileOfLabel.size(); i++) {
-			list.add(getLabelIndex(fileOfLabel.get(i)));
+			list.add(getLabelId(fileOfLabel.get(i)));
 		}
 		Collections.sort(list);
 		return list;
 	}
+
+	/**
+	 * 该方法可以接受一个选择的标签列表查询对应的标签id
+	 * 
+	 * @param fileOfLabel
+	 * @return [返回标签的id数组]
+	 * @throws Exception
+	 */
+	public ArrayList<Integer> checkLabelId(ArrayList<String> fileOfLabel)
+			throws Exception {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		String label;
+		int num = numberOfLine("src/label.txt");
+		for (int i = 0; i < fileOfLabel.size(); i++) {
+			for (int j = 1; j <= num; j++) {
+				label = readLine(j, "src/label.txt");
+				String[] labelArray = label.split("/");
+				label = fileOfLabel.get(i);
+				if (labelArray[1].equals(label)) {
+					list.add(Integer.valueOf(labelArray[0]));
+					break;
+				}
+			}
+		}
+		return list;
+	}
+
 	/**
 	 * 该方法将内容与标签的关系写入关系文件内
+	 * 
 	 * @param contentLabelList
-	 * @throws IOException
+	 * @throws Exception
 	 */
-	public void writeRelationship(ArrayList<Integer> contentLabelList)
-			throws IOException {
+	public void writeRelationship(String id, ArrayList<Integer> contentLabelList)
+			throws Exception {
 		BufferedWriter writeRelationship = new BufferedWriter(new FileWriter(
-				"D:\\1\\relationship.txt", true));
+				"src/relationship.txt", true));
+		writeRelationship.write(id + "/");
 		for (int i = 0; i < contentLabelList.size(); i++) {
 			writeRelationship.write(String.valueOf(contentLabelList.get(i))
 					+ ",");
@@ -378,20 +422,33 @@ public class Test extends JFrame implements ActionListener {
 		writeRelationship.newLine();
 		writeRelationship.close();
 	}
+
 	/**
 	 * 该方法读取关系文件内的关系
+	 * 
 	 * @param receiveRelationship
-	 * @return
+	 *            [需要读取的关系文件内的关系行]
+	 * @return [返回对应的关系数组]
 	 */
 	public String[] readRelationship(String receiveRelationship) {
+		String[] idList;
 		String[] relationship;
-		relationship = receiveRelationship.split(",");
+		idList = receiveRelationship.split("/");
+		if (idList.length == 2) {
+			relationship = idList[1].split(",");
+		} else {
+			relationship = new String[] { "" };
+		}
 		return relationship;
 	}
+
 	/**
 	 * 该方法判断查询时所选的标签与对应的关系文件的关系是否匹配
-	 * @param checkLabel [查询时所选择的标签]
-	 * @param fileLabel [关系文件内对应的关系]
+	 * 
+	 * @param checkLabel
+	 *            [查询时所选择的标签]
+	 * @param fileLabel
+	 *            [关系文件内对应的关系]
 	 * @return [匹配返回正确，反之不正确]
 	 */
 	public boolean judge(ArrayList<Integer> checkLabel, String[] fileLabel) {
@@ -423,30 +480,37 @@ public class Test extends JFrame implements ActionListener {
 		}
 		return flag;
 	}
+
 	/**
 	 * 该方法进行查询操作
-	 * @param contentLabelList [内容文件所有存储的内容]
+	 * 
+	 * @param checkLabelId
+	 *            [选择的标签]
 	 * @throws Exception
 	 */
-	public void request(ArrayList<Integer> contentLabelList) throws Exception {
+	public void request(ArrayList<Integer> checkLabelId) throws Exception {
 
 		boolean flag = false;
-		int num = numberOfLine("D:\\1\\relationship.txt");
-		if (num == 0) {
+		int relationshipNumber = numberOfLine("src/relationship.txt");
+		int contentNumber = numberOfLine("src/content.txt");
+		if (relationshipNumber == 0) {
 			JOptionPane.showMessageDialog(null, "系统内暂时没有存储内容", "错误",
 					JOptionPane.ERROR_MESSAGE);
 			txtInput.grabFocus();
 		} else {
-			for (int i = 0; i < num; i++) {
+			for (int i = 0; i < relationshipNumber; i++) {
 				if (judge(
-						contentLabelList,
-						readRelationship(readLine(i + 1,
-								"D:\\1\\relationship.txt")))) {
+						checkLabelId,
+						readRelationship(readLine(i + 1, "src/relationship.txt")))) {
+					String relationshipId = readLine(i + 1,
+							"src/relationship.txt").split("/")[0];
 					Pattern contentOfRequest = Pattern.compile(txtRequest
 							.getText());
-					String str = readLine(i + 1, "D:\\1\\content.txt");
+
+					String str = idMatch(relationshipId, contentNumber);
 					Matcher m = contentOfRequest.matcher(str);
 					if (m.find()) {
+						// String[] requestContent=str.split("/");
 						txtRequestContent.setText(str);
 						flag = true;
 						break;
@@ -461,67 +525,167 @@ public class Test extends JFrame implements ActionListener {
 			checkLabel.clear();
 		}
 	}
+
 	/**
 	 * 该方法在录入时进行查重操作
+	 * 
 	 * @return [有重复返回true]
 	 * @throws Exception
 	 */
 	public boolean checkRepeat() throws Exception {
 		boolean flag = false;
-		int num = numberOfLine("D:\\1\\content.txt");
+		int num = numberOfLine("src/content.txt");
 		for (int i = 1; i <= num; i++) {
-			if (txtInput.getText().equals(readLine(i, "D:\\1\\content.txt"))) {
+			if (txtInput.getText().equals(readLine(i, "src/content.txt"))) {
 				flag = true;
 				break;
 			}
 		}
 		return flag;
 	}
+
 	/**
 	 * 该方法进行删除操作
+	 * 
 	 * @throws Exception
 	 */
-	public void delete() throws Exception{
-		boolean flag=false;
-		   String contentOfDelete=txtDelete.getText();
-		   int index = 0,i;
-		   int num=numberOfLine("D:\\1\\content.txt");
-		   for(i=1;i<=num;i++){
-			   if(contentOfDelete.equals(readLine(i,"D:\\1\\content.txt"))){
-				   flag=true;
-				   index=i;
-				   break;
-			   }
-		   }
-		   if(flag==false){
-			   JOptionPane.showMessageDialog(null, "系统内不存在要删除的内容", "错误",
-						JOptionPane.ERROR_MESSAGE);
-				txtDelete.grabFocus();
-		   }
-		   else{
-			   ArrayList<String> deleteContent=new ArrayList<String>();
-			   ArrayList<String> deleteRelationship=new ArrayList<String>();
-			   for(i=1;i<=num;i++){
-				   deleteContent.add(readLine(i,"D:\\1\\content.txt"));
-				   deleteRelationship.add(readLine(i,"D:\\1\\relationship.txt"));
-			   }
-			   deleteContent.remove(index-1);
-			   deleteRelationship.remove(index-1);
-			   File content=new File("D:\\1\\content.txt");
-			   File relationship=new File("D:\\1\\relationship.txt");
-			   content.delete();
-			   relationship.delete();
-			   content.createNewFile();
-			   relationship.createNewFile();
-			   for(i=0;i<deleteContent.size();i++){
-				   write(deleteContent.get(i),"D:\\1\\content.txt");
-				   write(deleteRelationship.get(i),"D:\\1\\relationship.txt");
-			   }
-			   JOptionPane.showMessageDialog(null, "删除成功", "正确",
-						JOptionPane.PLAIN_MESSAGE);
-			   deleteContent.clear();
-			   deleteRelationship.clear();
-		   }
-		   txtDelete.setText("");
+	public void delete() throws Exception {
+		boolean flag = false;
+		String contentOfDelete = txtDelete.getText();
+		int index = 0, i, indexOfrelationship = 0;
+		String id = null;
+		int contentNumber = numberOfLine("src/content.txt");
+		int relationshipNumber = numberOfLine("src/relationship.txt");
+		for (i = 1; i <= contentNumber; i++) {
+			String[] contentArray = readLine(i, "src/content.txt").split("/");
+			if (contentOfDelete.equals(contentArray[1])) {
+				flag = true;
+				id = contentArray[0];
+				index = i;
+				break;
+			}
+		}
+		if (flag == false) {
+			JOptionPane.showMessageDialog(null, "系统内不存在要删除的内容", "错误",
+					JOptionPane.ERROR_MESSAGE);
+			txtDelete.grabFocus();
+		} else {
+			ArrayList<String> deleteContent = new ArrayList<String>();
+			ArrayList<String> deleteRelationship = new ArrayList<String>();
+			for (i = 1; i <= contentNumber; i++) {
+				deleteContent.add(readLine(i, "src/content.txt"));
+				deleteRelationship.add(readLine(i, "src/relationship.txt"));
+			}
+			for (i = 1; i <= relationshipNumber; i++) {
+				if (readLine(i, "src/relationship.txt").split("/")[0]
+						.equals(id)) {
+					indexOfrelationship = i;
+					break;
+				}
+			}
+			deleteContent.remove(index - 1);
+			deleteRelationship.remove(indexOfrelationship - 1);
+			File content = new File("src/content.txt");
+			File relationship = new File("src/relationship.txt");
+			content.delete();
+			relationship.delete();
+			content.createNewFile();
+			relationship.createNewFile();
+			for (i = 0; i < deleteContent.size(); i++) {
+				write(deleteContent.get(i), "src/content.txt");
+				write(deleteRelationship.get(i), "src/relationship.txt");
+			}
+			JOptionPane.showMessageDialog(null, "删除成功", "正确",
+					JOptionPane.PLAIN_MESSAGE);
+			deleteContent.clear();
+			deleteRelationship.clear();
+		}
+		txtDelete.setText("");
+	}
+
+	/**
+	 * 该方法得到标签的对应id
+	 * 
+	 * @param label
+	 *            [需要查询的标签]
+	 * @return [返回查询标签对应的id]
+	 * @throws Exception
+	 */
+	public int getLabelId(String label) throws Exception {
+		int id = 0;
+		String lineOfId;
+		int num = numberOfLine("src/label.txt");
+		for (int i = 1; i <= num; i++) {
+			lineOfId = readLine(i, "src/label.txt");
+			String[] idArray = lineOfId.split("/");
+			if (idArray[1].equals(label)) {
+				id = Integer.valueOf(idArray[0]);
+				break;
+			}
+		}
+		return id;
+	}
+
+	/**
+	 * 该方法创建一个新Id
+	 * 
+	 * @param fileName
+	 *            [要增加Id的文件名]
+	 * @return [返回一个新Id]
+	 * @throws Exception
+	 */
+	public String creatId(String fileName) throws Exception {
+		String id;
+		String[] idArray;
+		int num = numberOfLine(fileName);
+		if (num != 0) {
+			String lineOfId = readLine(num, fileName);
+			idArray = lineOfId.split("/");
+			int i = Integer.valueOf(idArray[0]).intValue() + 1;
+			id = String.valueOf(i);
+		} else {
+			id = String.valueOf(1);
+		}
+		return id;
+	}
+
+	/**
+	 * 该方法匹配内容Id和关系Id
+	 * 
+	 * @param relationshipId
+	 *            [关系的内容id]
+	 * @param contentNumber
+	 *            [内容文件的行数]
+	 * @return [返回匹配成功后的内容]
+	 * @throws Exception
+	 */
+	public String idMatch(String relationshipId, int contentNumber)
+			throws Exception {
+		String content = null;
+		for (int i = 1; i <= contentNumber; i++) {
+			String[] contentId = readLine(i, "src/content.txt").split("/");
+			if (relationshipId.equals(contentId[0])) {
+				content = contentId[1];
+				break;
+			} else {
+				continue;
+			}
+		}
+		return content;
+	}
+
+	public void judgeFile() throws IOException {
+		File content = new File("src/content.txt");
+		File label = new File("src/label.txt");
+		File relationship = new File("src/relationship.txt");
+		if (!content.exists()) {
+			content.createNewFile();
+		}
+		if (!label.exists()) {
+			label.createNewFile();
+		}
+		if (!relationship.exists()) {
+			relationship.createNewFile();
+		}
 	}
 }
